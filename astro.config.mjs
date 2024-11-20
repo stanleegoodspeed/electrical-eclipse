2d3d77f85ecfee90ffdec9b48b8a941126f1fb29
Colin Cole <ccole3390@gmail.com>
2024-11-19 21:06:09 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+export default defineConfig({
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
